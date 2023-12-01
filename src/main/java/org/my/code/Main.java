11d5a58773eecda60520e4826268ecdaee7da251
lizhao9 <lizhao9@lenovo.com>
2023-12-01 11:33:51 +0800

    code

--- src/main/java/org/my/code/Main.java
+++ src/main/java/org/my/code/Main.java
@@ -1,2 +1,7 @@
-package org.my.code;public class Main {
+package org.my.code;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
